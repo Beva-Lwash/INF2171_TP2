f477f35e074f7f79ab273718a8e5363448878d7f
Ahmadou <seye.ahmadou@courrier.uqam.ca>
2019-11-03 06:00:25 -0500

    Ajout de modification de Samira

--- src/batnav.java
+++ src/batnav.java
@@ -1,52 +1,108 @@
-import java.util.Scanner;
 
 public class batnav {
+    public static String reccupererEntreeClavier()
+    {
+        String DEMANDE_D_ENTREE = "Entrer la description et la position des bateaux \r\n"
+                        + "selon le format suivant, separes par des espaces: \r\n"
+                        + "taille[p/m/g] orientation[h/v] colonne[A-R] rang√©e[1-9] \r\n"
+                        + "ex: ghC4 mvM2 phK9:\r\n";
 
-	public static void main(String[] args) {
+        Pep8.charo('\n');
 
-		String Bienvenue = "Bienvenue dans ce jeu de bataille navale!\n";
-		Pep8.stro(Bienvenue);
+         Pep8.stro(DEMANDE_D_ENTREE);
+         char entreeUsercarc;
+         String entreeBateau ="";
+        do{
+             entreeUsercarc=Pep8.chari();
+             entreeBateau = entreeBateau + entreeUsercarc;
+         }while(entreeUsercarc != '\n');   
 
-		String DEMANDE_D_ENTREE = "Entrer la description et la position des bateaux \r\n"
-				+ "selon le format suivant, separes par des espaces: \r\n"
-				+ "taille[p/m/g] orientation[h/v] colonne[A-R] rangÈe[1-9] \r\n";
+        return entreeBateau;
+    }    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        String Bienvenue = "Bienvenue dans ce jeu de bataille navale!\n";
+        Pep8.stro(Bienvenue);
 
-		String Colonnes = "ABCDEFGHIJKLMNOPQR";
-		// TODO crÈÈr tableau
-		char[][] matrice= new char[9][18];
+        String DEMANDE_D_ENTREE = "Entrer la description et la position des bateaux \r\n"
+                        + "selon le format suivant, separes par des espaces: \r\n"
+                        + "taille[p/m/g] orientation[h/v] colonne[A-R] rang√©e[1-9] \r\n"
+                        + "ex: ghC4 mvM2 phK9:\r\n";
 
-		for (int i = 0; i <= 8; i++) {
-			for (int j = 0; j <= 17; j++) {
-				matrice[i][j]='~';
-			}
-		}
-		
-		Pep8.charo(' ');
-		Pep8.charo(' ');
-		Pep8.stro(Colonnes);
-		Pep8.charo('\n');
-		for (int i = 0; i <= 8; i++) {
-			Pep8.deco(i+1);
-			Pep8.charo('|');
-			for (int j = 0; j <= 17; j++) {
-				Pep8.charo(matrice[i][j]);
-			}
-			Pep8.charo('|');
-			Pep8.charo('\n');
-		}
-		// TODO ajouts des bateaux
-		Pep8.charo('\n');
-		
-		Pep8.stro(DEMANDE_D_ENTREE);
+        
+        String ERREUR_ENTREE = "Le format de l'entrÈe ne respecte pas les paramËtres indiquÈs!";
 
-		// TODO ajouts des diffÈrents coups
+        String Colonnes = "  ABCDEFGHIJKLMNOPQR";
+        //Grandeur
+        char p = 'p';    //petit
+        char m = 'm';   //moyen
+        char g = 'g';   //grand
+        //Orientation
+        char h = 'h';  //horizontal
+        char v = 'v';  //vertical
+        // TODO cr√©√©r tableau
+        char[][] matrice= new char[9][18];
 
-		// TODO gÈrer les ricochets
+        for (int i = 0; i <= 8; i++) {
+                for (int j = 0; j <= 17; j++) {
+                        matrice[i][j]='~';
+                }
+        }
 
-		// TODO gÈrer la rÈcursivitÈ
+        Pep8.stro(Colonnes);
+        Pep8.charo('\n');
+        for (int i = 0; i <= 8; i++) {
+                Pep8.deco(i+1);
+                Pep8.charo('|');
+                for (int j = 0; j <= 17; j++) {
+                    Pep8.charo(matrice[i][j]);
+                }
+                Pep8.charo('|');
+                Pep8.charo('\n');
+        }
+        // TODO ajouts des bateaux
+            String entreeBateau = reccupererEntreeClavier();
+           // Extraire les 5 premiers caracteres
+           //int lengthentree =0;
+           if(entreeBateau.length() >4){
+                String bateau = entreeBateau.substring(0,4);
+                ///System.out.println("bateau = " + bateau);
+                
+                //verifier si l entree correspond aux parametres demandes
+                
+                char grandeurentree = bateau.charAt(0);
+                char orientationentree = bateau.charAt(1);
+                char colentree = bateau.charAt(2);
+                char rangeentree = bateau.charAt(2);
+                
+                if(!(grandeurentree == p || grandeurentree == m || grandeurentree == g)){
+                    System.err.println(ERREUR_ENTREE);
+                    Pep8.charo('\n');
+                    reccupererEntreeClavier();
+                    //Pep8.chari();
+                }
 
-		Pep8.stop();
+           }else{               
+                Pep8.stro(ERREUR_ENTREE);
+                Pep8.stro(DEMANDE_D_ENTREE);
+           }
+           //Valider le format de l'entree
+           //taille[p/m/g] orientation[h/v] colonne[A-R] rang√©e[1-9] puis un espace
+
+
+
+            // TODO ajouts des diff√©rents coups
+
+            // TODO g√©rer les ricochets
+
+            // TODO g√©rer la r√©cursivit√©
+
+            Pep8.stop();
 
 	}
-
+    
 }
